b42157ad69e219df5b8426e43b5292998e101126
DruzhinaAnna <91753354+DruzhinaAnna@users.noreply.github.com>
2021-11-06 23:37:55 +0300

    Add files via upload

---
+++ LR29/Test/Test/Test.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    float a, b, c, P;
+    cout << "\n a=";
+    cin >> a;
+    cout << "\n b=";
+    cin >> b;
+    cout << "\n c=";
+    cin >> c;
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        P = a + b + c;
+        cout << "\n Периметр равен=" << P;
+    }
+    else
+        cout << "\n Неверные исходные данные";
+}
