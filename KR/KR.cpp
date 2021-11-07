a94a2025331a584a7d5b896bc5eda0d67389562b
DruzhinaAnna <91753354+DruzhinaAnna@users.noreply.github.com>
2021-11-07 20:18:37 +0300

    Add files via upload

---
+++ KR/KR.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sys/stat.h>
+using namespace std;
+int main()
+{
+	int fd, x;
+	char buffer[1024];
+	string text;
+	char file;
+	cout << "Введите 1 (прочитать из STDIN_FILENO) или 2 (прочитать из файла): ";
+	cin >> x;
+	while (x != 1 && x != 2)
+	{
+		cout << "Вы ввели неправильное значение. Введите снова:  ";
+		cin >> x;
+	}
+	switch (x)
+	{
+		case 1: 
+			cout << "\nВведите текст:\n";
+			if (read(STDIN_FILENO, buffer, 1025) == EOF) 
+			{
+				printf("Чтение произвелось до конца файла\n"); 
+				exit(1);
+			}
+			else
+			{
+				text = buffer;
+				cout << "Kоличество прочитанных байт: " << text.length() - 1 << endl;
+				break;
+			}
+		}
+		case 2: 
+		{
+			if ((fd = open("file.txt", O_RDONLY)) == -1) 
+			{
+				printf("Невозможно открыть файл\n");
+				exit(1);
+			}
+			if (read(fd, buffer, 1025) !=-1) 
+				cout << "Чтение файла произведено до конца\n";
+			text = buffer;
+			cout << "\nПрочитанный текст:\n"<<text<<endl;
+			cout << "Kоличество прочитанных байт: " << text.length()-1<<endl;
+			break;
+		}
+	}
+	return 0;
+}
